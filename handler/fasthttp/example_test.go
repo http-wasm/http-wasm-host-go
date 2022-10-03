f22e289e67b54da79f1f92c3598319af8f4bf768
Crypt Keeper <64215+codefromthecrypt@users.noreply.github.com>
2022-10-03 04:06:59 +0200

    Adds fasthttp handler (#3)
    * Adds set_response_header; tunes read_request_header and send_response
    
    Signed-off-by: Adrian Cole <adrian@tetrate.io>
    
    Co-authored-by: Anuraag Agrawal <anuraaga@gmail.com>

---
+++ handler/fasthttp/example_test.go
@@ -0,0 +1,133 @@
+package wasm
+
+import (
+	"context"
+	_ "embed"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"net/http"
+
+	"github.com/valyala/fasthttp"
+
+	httpwasm "github.com/http-wasm/http-wasm-host-go"
+	"github.com/http-wasm/http-wasm-host-go/internal/test"
+)
+
+var serveJson = func(ctx *fasthttp.RequestCtx) {
+	ctx.Request.Header.Set("Content-Type", "application/json")
+	ctx.WriteString("{\"hello\": \"world\"}") // nolint
+	ctx.SetStatusCode(200)
+}
+
+func Example_auth() {
+	ctx := context.Background()
+
+	// Configure and compile the WebAssembly guest binary. In this case, it is
+	// an auth interceptor.
+	mw, err := NewMiddleware(ctx, test.AuthWasm)
+	if err != nil {
+		log.Panicln(err)
+	}
+	defer mw.Close(ctx)
+
+	// Wrap the real handler with an interceptor implemented in WebAssembly.
+	wrapped, err := mw.NewHandler(ctx, serveJson)
+	if err != nil {
+		log.Panicln(err)
+	}
+
+	// Start the server with the wrapped handler.
+	ts, url := listenAndServe(wrapped)
+	defer ts.Shutdown()
+
+	// Invoke some requests, only one of which should pass
+	headers := []http.Header{
+		{"NotAuthorization": {"1"}},
+		{"Authorization": {""}},
+		{"Authorization": {"Basic QWxhZGRpbjpvcGVuIHNlc2FtZQ=="}},
+		{"Authorization": {"0"}},
+	}
+
+	for _, header := range headers {
+		req, err := http.NewRequest(http.MethodGet, url, nil)
+		if err != nil {
+			log.Panicln(err)
+		}
+		req.Header = header
+
+		resp, err := http.DefaultClient.Do(req)
+		if err != nil {
+			log.Panicln(err)
+		}
+		resp.Body.Close()
+
+		switch resp.StatusCode {
+		case http.StatusOK:
+			fmt.Println("OK")
+		case http.StatusUnauthorized:
+			fmt.Println("Unauthorized")
+		default:
+			log.Panicln("unexpected status code", resp.StatusCode)
+		}
+	}
+
+	// Output:
+	// Unauthorized
+	// Unauthorized
+	// OK
+	// Unauthorized
+}
+
+func Example_log() {
+	ctx := context.Background()
+	logger := func(_ context.Context, msg string) { fmt.Println(msg) }
+
+	// Configure and compile the WebAssembly guest binary. In this case, it is
+	// a logging interceptor.
+	mw, err := NewMiddleware(ctx, test.LogWasm, httpwasm.Logger(logger))
+	if err != nil {
+		log.Panicln(err)
+	}
+	defer mw.Close(ctx)
+
+	// Wrap the real handler with an interceptor implemented in WebAssembly.
+	wrapped, err := mw.NewHandler(ctx, serveJson)
+	if err != nil {
+		log.Panicln(err)
+	}
+
+	// Start the server with the wrapped handler.
+	ts, url := listenAndServe(wrapped)
+	defer ts.Shutdown()
+
+	// Make a client request and print the contents to the same logger
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Panicln(err)
+	}
+	defer resp.Body.Close()
+	content, _ := io.ReadAll(resp.Body)
+	logger(ctx, string(content))
+
+	// Output:
+	// before
+	// after
+	// {"hello": "world"}
+}
+
+func listenAndServe(wrapped RequestHandler) (*fasthttp.Server, string) {
+	ts := &fasthttp.Server{Handler: wrapped.Handle}
+	ln, err := net.Listen("tcp4", "127.0.0.1:")
+	if err != nil {
+		log.Panicln(err)
+	}
+	url := fmt.Sprintf("http://%s", ln.Addr().String())
+	go func() {
+		if err = ts.Serve(ln); err != nil {
+			log.Panicln(err)
+		}
+	}()
+	return ts, url
+}
---
+++ handler/fasthttp/go.mod
@@ -0,0 +1,17 @@
+module github.com/http-wasm/http-wasm-host-go/handler/fasthttp
+
+go 1.18
+
+require (
+	github.com/http-wasm/http-wasm-host-go v0.0.0
+	github.com/valyala/fasthttp v1.40.0
+)
+
+require (
+	github.com/andybalholm/brotli v1.0.4 // indirect
+	github.com/klauspost/compress v1.15.0 // indirect
+	github.com/tetratelabs/wazero v1.0.0-pre.2 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+)
+
+replace github.com/http-wasm/http-wasm-host-go => ../../
